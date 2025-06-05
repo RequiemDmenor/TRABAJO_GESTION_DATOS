010ced0db996163ae110143b43bdcdda518f5cfb
RequiemDmenor <99396270+RequiemDmenor@users.noreply.github.com>
2025-06-05 23:31:38 +0200

    ACTUALIZACION EDROOM

--- uah_photsat_freertos/asw/components/ccobsmng/include/public/ccobsmng_iface_v1.h
+++ uah_photsat_freertos/asw/components/ccobsmng/include/public/ccobsmng_iface_v1.h
@@ -14,6 +14,12 @@
 #include <public/cdtchandler_iface_v1.h>
 
 
+//******************************************************************************
+// Required software interfaces
+
+#include <public/pus_services_iface_v1.h>
+
+
 /**
  * \class   CCObsMng
  *
